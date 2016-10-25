89df9897248a3599d37ce0b906819f142953d20e
gituser <gauravanand.2008@gmail.com>
2016-10-25 15:55:11 -0700

    second changes done on 25 oct

--- first.pp
+++ first.pp
@@ -1,7 +1,7 @@
 $pup='/root/puppet/puppet-code'
+$message='hello gaurav'
 file{"$pup/out.txt":
   ensure=>'present',
   mode=>'0755',
-  content=>'hello
-            world',
+  content=>"$message",
 }
